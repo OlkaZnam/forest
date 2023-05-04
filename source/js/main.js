56460424f1c871a04839b5e7abd2714e18a96240
Olka <lunar.raccoon@yandex.ru>
2023-05-04 10:16:27 +0900

    add slider buttons

--- source/js/main.js
+++ source/js/main.js
@@ -1,32 +1,32 @@
 const initCategorySlider = () => {
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        // direction: 'vertical',
-        // loop: true,
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    // loop: true,
 
-        // If we need pagination
-        // pagination: {
-        //     el: '.swiper-pagination',
-        // },
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
 
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-        keyboard: {
-            enabled: true,
-            onlyInViewport: true,
-            pageUpDown: true,
-        }
-    })
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+    }
+  })
 }
 
 const initSliders = () => {
-    initCategorySlider()
+  initCategorySlider()
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    initSliders()
+  initSliders()
 })
